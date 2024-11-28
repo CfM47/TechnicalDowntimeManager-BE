a0e0773d30f385a8acb107c177b7d0d7f3139c4d
LoLProM <mauriciosundejimenez@gmail.com>
2024-11-28 12:02:27 -0500

    adding rate feature

--- src/features/Equipment/utils.ts
+++ src/features/Equipment/utils.ts
@@ -4,5 +4,5 @@ export const equipmentSchema = z.object({
   name: z.string().max(255),
   type: z.string().max(255),
   state: z.string().max(255),
-  id_department: z.string().uuid(),
+  id_department: z.string().uuid()
 });
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import { Models } from './utils';
 import { UserModel } from './features/User/model';
 import { TechnicianModel } from './features/Technician/model';
 import { EquipmentModel } from './features/Equipment/model';
+import { RateModel } from './features/Rate/model';
 
 const appModels: Models = {
   userModel: new UserModel(),
   technicianModel: new TechnicianModel(),
-  equipmentModel: new EquipmentModel()
+  equipmentModel: new EquipmentModel(),
+  rateModel: new RateModel()
 };
 
 createApp(appModels);
