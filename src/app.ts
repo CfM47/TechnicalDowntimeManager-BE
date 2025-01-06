5d94ce4ed90b0c452b56ab44cb52da661be57537
LoLProM <mauriciosundejimenez@gmail.com>
2025-01-06 15:10:24 -0500

    adding swagger for API documentation

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import { appRouter } from './router';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerConfig from '../swaggerConfig';
 import { Models } from './utils';
 
 dotenv.config();
@@ -13,11 +16,13 @@ export const createApp = (appModels: Models) => {
   app.disable('x-powered-by');
 
   const port = process.env.PORT || 3000;
-  const host_url = process.env.HOST_URL;
 
+  const swaggerSpec = swaggerJsdoc(swaggerConfig);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   app.use('/api', appRouter(appModels));
 
   app.listen(port, () => {
-    console.log(`Server is running on port ${host_url}:${port}`);
+    console.log(`Server is running on port http://localhost:${port}`);
+    console.log(`Swagger docs are available at http://localhost:${port}/api-docs`);
   });
 };
